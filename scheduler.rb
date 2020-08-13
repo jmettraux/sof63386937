f93dc9095cb0c561aca5e497b11c78d6a653f99d
John Mettraux <jmettraux@gmail.com>
2020-08-13 16:48:32 +0900

    Reenable task

--- scheduler.rb
+++ scheduler.rb
@@ -7,6 +7,8 @@ Rufus::Scheduler.singleton.every '10s' do
   p [ :run, :load_organizations, Time.now ]
   Rake.load_rakefile(File.join(__dir__, 'Rakefile'))
   Rake::Task['load_organizations'].invoke
+  Rake::Task['load_organizations'].reenable
+    # thanks to https://til.hashrocket.com/posts/4897ed42b7-invoking-rake-tasks-multiple-times
 end
 Rufus::Scheduler.singleton.every '5s' do
   p [ :every_5s, Time.now ]
